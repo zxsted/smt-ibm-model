cdfe88f0ffb5bfd954745f5d4fb961f581060bb4
Phong Tuan Nguyen <tuanphong94@gmail.com>
2016-05-31 17:54:22 +0700

    add copyright

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-### IBM Model 1&2
+### IBM Models 1, 2 and 3
+
+This is a Java-implemented version of IBM Models 1, 2 and 3
+which is written in Java by Nguyen Tuan Phong (VNU-UET) under
+the instruction of Assoc. Prof. Le Anh Cuong (Ton Duc Thang University).
--- src/vn/edu/vnu/uet/nlp/smt/ibm/IBMModel3.java
+++ src/vn/edu/vnu/uet/nlp/smt/ibm/IBMModel3.java
@@ -1,5 +1,31 @@
+/*******************************************************************************
+ * Copyright [2016] [Nguyen Tuan Phong]
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package vn.edu.vnu.uet.nlp.smt.ibm;
 
-public class IBMModel3 {
-	
+public class IBMModel3 extends IBMModel2 {
+
+	public IBMModel3(String enFile, String foFile) {
+		super(enFile, foFile);
+	}
+
+	public IBMModel3(String model) {
+		super(model);
+	}
+
+	@Override
+	public void train() {
+		super.train();
+	}
 }
